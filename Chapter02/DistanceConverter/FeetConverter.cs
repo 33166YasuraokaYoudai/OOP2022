57cb767136ff7d45c6172aad554226d6d42896e3
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-04-22 15:53:38 +0900

    変更

--- Chapter02/DistanceConverter/FeetConverter.cs
+++ Chapter02/DistanceConverter/FeetConverter.cs
@@ -11,12 +11,12 @@ namespace DistanceConverter {
 
         //メートルからフィートを求める
         public double FromMeter(double meter) {
-            return meter * 0.3048;
+            return meter / 0.3048;
         }
 
         //フィートからメートルを求める
         public double ToMeter(double feet) {
-            return feet / 0.3048;
+            return feet * 0.3048;
         }
 
     }
