cdcdcd7714c01af398271f0bfcf59e136e125ec3
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-04-28 15:51:15 +0900

    インターフェイスの実装

--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SalesCalculator {
     public class SalesCounter {
-        private List<Sale> _sales; //csvファイルから読み込んだデータ
+        private IEnumerable<Sale> _sales; //csvファイルから読み込んだデータ
 
         //コンストラクタ
         public SalesCounter(string filePath) {
@@ -16,7 +16,7 @@ namespace SalesCalculator {
         }
 
         //店舗別売り上げを求める
-        public Dictionary<string, int> GetPerStoreSales() {
+        public IDictionary<string, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (Sale sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
@@ -28,7 +28,7 @@ namespace SalesCalculator {
             return dict;
         }
         //売り上げデータを読み込み、Saleオブジェクトのリストを繰り返す
-        public static List<Sale> ReadSales(String filePath) {
+        public static IEnumerable<Sale> ReadSales(String filePath) {
             List<Sale> sales = new List<Sale>();
             String[] lines = File.ReadAllLines(filePath);
             foreach (string line in lines) {
