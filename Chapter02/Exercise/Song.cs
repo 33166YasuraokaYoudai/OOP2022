aed3835a0de02ab5ac556b7dd9ef47b4aa90ad96
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-05-10 13:41:57 +0900

    演習問題2.1.1 完成

---
+++ Chapter02/Exercise/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise {
+    class Song {
+
+        public string title { get; set; } //歌のタイトル
+
+        public string ArtistName { get; set; } //アーティスト名
+
+        public int Length { get; set; } //演奏時間（単位は秒）
+
+
+    }
+}
