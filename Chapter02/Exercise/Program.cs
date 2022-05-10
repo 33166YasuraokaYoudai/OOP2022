bef7c34e26a0fb1805642d9c9473dc44ef4f1e8a
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-05-10 15:54:21 +0900

    問題2.1.4 完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -8,10 +8,29 @@ namespace Exercise {
     class Program {
         static void Main(string[] args) {
 
+            /*
             var songs = new List<Song>();
 
-            Song song = new Song("歌のタイトル","アーティスト名","秒");
+            var song1 = new Song("歌のタイトル","アーティスト名",1);
+            songs.Add(song1);
+            var song2 = new Song("歌のタイトル", "アーティスト名", 1);
+            songs.Add(song2);
+            var song3 = new Song("歌のタイトル", "アーティスト名", 1);
+            songs.Add(song3);
+            */
+            var songs = new Song[] {
+                new Song ("ああああ","アーティスト名11",180),
+                new Song ("いいいい","アーティスト名22",240),
+                new Song ("うううう","アーティスト名33",60),
+                new Song ("ええええ","アーティスト名44",600),
+            };
+            PrintSongs(songs);
+        }
+        private static void PrintSongs(Song[] songs) {
+            foreach (var song in songs) {
+                Console.WriteLine("{0},{1},{2:m\\:ss}", song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
 
+            }
         }
     }
 }
