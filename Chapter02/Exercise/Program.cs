87fe4722343aa488da38a365b9a1cc1bdf2c7e71
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-05-12 14:02:59 +0900

    変更

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -11,11 +11,11 @@ namespace Exercise {
             /*
             var songs = new List<Song>();
 
-            var song1 = new Song("歌のタイトル","アーティスト名",1);
+            var song1 = new Song("ああああ","アーティスト名11",180);
             songs.Add(song1);
-            var song2 = new Song("歌のタイトル", "アーティスト名", 1);
+            var song2 = new Song("いいいい","アーティスト名22",240);
             songs.Add(song2);
-            var song3 = new Song("歌のタイトル", "アーティスト名", 1);
+            var song3 = new Song("うううう","アーティスト名33",60);
             songs.Add(song3);
             */
             var songs = new Song[] {
@@ -26,7 +26,7 @@ namespace Exercise {
             };
             PrintSongs(songs);
         }
-        private static void PrintSongs(Song[] songs) {
+        private static void PrintSongs(IEnumerable<Song> songs) {
             foreach (var song in songs) {
                 Console.WriteLine("{0},{1},{2:m\\:ss}", song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
 
