5279c86112e325db1de2001a2c5ac1a2619a7d1b
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-05-13 15:17:08 +0900

    ラムダ演算子

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+
+
+        static void Main(string[] args) {
+
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            //一致する要素があるかないか
+            var exists = list.Exists( s => s[0] == 'A');
+            Console.WriteLine(exists);
+
+            //条件と一致する要素を検索し最初に見つかった要素を返す
+            var name = list.Find(s => s.Length == 6);
+            Console.WriteLine(name);
+
+            //一致するすべての要素を返す
+            var names = list.FindAll(s => s.Length <= 5);
+            names.ForEach(s => Console.WriteLine(s));
+            
+        }
+    }
+}
