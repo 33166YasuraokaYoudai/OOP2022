e01ab13dd0de29c1aaafd1ae251a92d47fcd4c8f
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-05-26 15:48:31 +0900

    問題5.2 完成

---
+++ Chapter05/Exercise02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            var intstr = Console.ReadLine();
+
+
+            if (int.TryParse(intstr, out var IntStr)) {
+                Console.WriteLine("{0:N}", IntStr);
+            } else {
+                Console.WriteLine("無理");
+            }
+        }
+    }
+}
