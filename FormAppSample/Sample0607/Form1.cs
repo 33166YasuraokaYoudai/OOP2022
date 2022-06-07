8c6bb9d05458cb3728f604b7203f5ce18e1f3bab
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-06-07 14:49:15 +0900

    Sampleプログラム

---
+++ FormAppSample/Sample0607/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0607 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+
+            if(tbNum2.Text == 0.ToString()){
+                MessageBox.Show("計算できない","エラー",MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+            } else {
+                nudAns.Value = nudNum1.Value / nudNum2.Value;
+                nudMod.Value = nudNum1.Value % nudNum2.Value;
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) {
+
+        }
+    }
+}
