7f71bcb0b51130a7d9cd004b9a6c9cd20e9e13b6
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-06-14 14:53:49 +0900

    統一試験　Test01 完成

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,36 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
 
-
-
-            
         }
-
-
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+            List<Student> scores = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student std = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                scores.Add(std);
+            }
+            return scores;
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score) {
+                if (dict.ContainsKey(score.Subject))
+                    dict[score.Subject] += score.Score;
+                else
+                    dict[score.Subject] = score.Score;
 
-
-
-
-
-            
+            }
+            return dict;
         }
     }
 }
