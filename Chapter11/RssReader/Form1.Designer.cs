60c68c76ab77c2583aa1cc0efe4d189f1296015d
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-06-28 15:57:24 +0900

    6/28 途中経過

---
+++ Chapter11/RssReader/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace RssReader {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.cbRssUrl = new System.Windows.Forms.ComboBox();
+            this.btRssGet = new System.Windows.Forms.Button();
+            this.lbRssTitle = new System.Windows.Forms.ListBox();
+            this.wbBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // cbRssUrl
+            // 
+            this.cbRssUrl.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbRssUrl.FormattingEnabled = true;
+            this.cbRssUrl.Location = new System.Drawing.Point(12, 24);
+            this.cbRssUrl.Name = "cbRssUrl";
+            this.cbRssUrl.Size = new System.Drawing.Size(786, 27);
+            this.cbRssUrl.TabIndex = 0;
+            // 
+            // btRssGet
+            // 
+            this.btRssGet.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btRssGet.Location = new System.Drawing.Point(815, 23);
+            this.btRssGet.Name = "btRssGet";
+            this.btRssGet.Size = new System.Drawing.Size(107, 27);
+            this.btRssGet.TabIndex = 1;
+            this.btRssGet.Text = "取得";
+            this.btRssGet.UseVisualStyleBackColor = true;
+            this.btRssGet.Click += new System.EventHandler(this.btRssGet_Click);
+            // 
+            // lbRssTitle
+            // 
+            this.lbRssTitle.FormattingEnabled = true;
+            this.lbRssTitle.ItemHeight = 12;
+            this.lbRssTitle.Location = new System.Drawing.Point(21, 71);
+            this.lbRssTitle.Name = "lbRssTitle";
+            this.lbRssTitle.Size = new System.Drawing.Size(365, 712);
+            this.lbRssTitle.TabIndex = 2;
+            this.lbRssTitle.SelectedIndexChanged += new System.EventHandler(this.lbRssTitle_SelectedIndexChanged);
+            // 
+            // wbBrowser
+            // 
+            this.wbBrowser.Location = new System.Drawing.Point(401, 71);
+            this.wbBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbBrowser.Name = "wbBrowser";
+            this.wbBrowser.Size = new System.Drawing.Size(510, 712);
+            this.wbBrowser.TabIndex = 3;
+            this.wbBrowser.Url = new System.Uri("https://www.yahoo.co.jp/", System.UriKind.Absolute);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 800);
+            this.Controls.Add(this.wbBrowser);
+            this.Controls.Add(this.lbRssTitle);
+            this.Controls.Add(this.btRssGet);
+            this.Controls.Add(this.cbRssUrl);
+            this.Name = "Form1";
+            this.Text = "RSSReader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbRssUrl;
+        private System.Windows.Forms.Button btRssGet;
+        private System.Windows.Forms.ListBox lbRssTitle;
+        private System.Windows.Forms.WebBrowser wbBrowser;
+    }
+}
+
---
+++ Chapter11/RssReader/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+
+        List<string> xTitle = new List<string>();
+        List<string> xLink = new List<string>();
+
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btRssGet_Click(object sender, EventArgs e) {
+
+            using (var wc = new WebClient()) {
+                var stream = wc.OpenRead(cbRssUrl.Text);
+
+                var xdoc = XDocument.Load(stream);
+                var xTitle = xdoc.Root.Descendants("item").Select(a => (string)a.Element("title"));
+                var xLink = xdoc.Root.Descendants("item").Select(a => (string)a.Element("link"));
+                foreach (var RssData in xTitle) {
+                    lbRssTitle.Items.Add(RssData);
+                }
+            }
+        }
+
+        private void lbRssTitle_SelectedIndexChanged(object sender, EventArgs e) {
+            var getindex = lbRssTitle.SelectedIndex;
+           
+        }
+    }
+}
