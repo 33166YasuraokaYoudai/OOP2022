7227f8ed5b2d350471551e7a41e63855db440475
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-07-01 15:59:13 +0900

    7/1 ここまで

--- Chapter11/RssReader/Form1.cs
+++ Chapter11/RssReader/Form1.cs
@@ -13,8 +13,7 @@ using System.Xml.Linq;
 namespace RssReader {
     public partial class Form1 : Form {
 
-        List<string> xTitle = new List<string>();
-        List<string> xLink = new List<string>();
+        List<string> link = new List<string>();
 
 
         public Form1() {
@@ -29,15 +28,41 @@ namespace RssReader {
                 var xdoc = XDocument.Load(stream);
                 var xTitle = xdoc.Root.Descendants("item").Select(a => (string)a.Element("title"));
                 var xLink = xdoc.Root.Descendants("item").Select(a => (string)a.Element("link"));
-                foreach (var RssData in xTitle) {
+                lbRssTitle.Items.Clear();
+                foreach (var RssData in xTitle) { 
                     lbRssTitle.Items.Add(RssData);
                 }
+                foreach (var linklist in xLink) {
+                    link.Add(linklist);
+                }
+                
             }
         }
 
         private void lbRssTitle_SelectedIndexChanged(object sender, EventArgs e) {
             var getindex = lbRssTitle.SelectedIndex;
+            wvBrowser.Navigate(link[getindex]);
            
+        }
+
+        private void btBack_Click(object sender, EventArgs e) {
+            wvBrowser.GoBack();
+        }
+
+        private void btForward_Click(object sender, EventArgs e) {
+            wvBrowser.GoForward();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            
+            btBack.Enabled = wvBrowser.CanGoBack;
+            btForward.Enabled = wvBrowser.CanGoForward;
+
+        }
+
+        private void wvBrowser_NavigationCompleted(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationCompletedEventArgs e) {
+            btBack.Enabled = wvBrowser.CanGoBack;
+            btForward.Enabled = wvBrowser.CanGoForward;
         }
     }
 }
