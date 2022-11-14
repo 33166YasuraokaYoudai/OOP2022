639c6d07d0b5ffd06b41ed86ab7780feded96009
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-11-14 15:56:15 +0900

    天気予報アプリ　途中経過 11/14

--- Chapter14/WeatherApp/Form1.Designer.cs
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -24,14 +24,16 @@ namespace WeatherApp {
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tbWeatherInfo = new System.Windows.Forms.TextBox();
             this.btWeatherGet = new System.Windows.Forms.Button();
-            this.cbPrefecture = new System.Windows.Forms.ComboBox();
+            this.cbArea = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // tbWeatherInfo
             // 
-            this.tbWeatherInfo.Location = new System.Drawing.Point(12, 79);
+            this.tbWeatherInfo.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbWeatherInfo.Location = new System.Drawing.Point(612, 482);
             this.tbWeatherInfo.Multiline = true;
             this.tbWeatherInfo.Name = "tbWeatherInfo";
             this.tbWeatherInfo.Size = new System.Drawing.Size(595, 293);
@@ -40,7 +42,7 @@ namespace WeatherApp {
             // btWeatherGet
             // 
             this.btWeatherGet.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btWeatherGet.Location = new System.Drawing.Point(12, 25);
+            this.btWeatherGet.Location = new System.Drawing.Point(22, 25);
             this.btWeatherGet.Name = "btWeatherGet";
             this.btWeatherGet.Size = new System.Drawing.Size(97, 39);
             this.btWeatherGet.TabIndex = 1;
@@ -48,26 +50,28 @@ namespace WeatherApp {
             this.btWeatherGet.UseVisualStyleBackColor = true;
             this.btWeatherGet.Click += new System.EventHandler(this.btWeatherGet_Click);
             // 
-            // cbPrefecture
+            // cbArea
             // 
-            this.cbPrefecture.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.cbPrefecture.FormattingEnabled = true;
-            this.cbPrefecture.Location = new System.Drawing.Point(163, 29);
-            this.cbPrefecture.Name = "cbPrefecture";
-            this.cbPrefecture.Size = new System.Drawing.Size(306, 32);
-            this.cbPrefecture.TabIndex = 2;
-            this.cbPrefecture.SelectedIndexChanged += new System.EventHandler(this.cbPrefecture_SelectedIndexChanged);
+            this.cbArea.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbArea.FormattingEnabled = true;
+            this.cbArea.Location = new System.Drawing.Point(163, 29);
+            this.cbArea.Name = "cbArea";
+            this.cbArea.Size = new System.Drawing.Size(354, 32);
+            this.cbArea.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cbPrefecture);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1229, 788);
+            this.Controls.Add(this.cbArea);
             this.Controls.Add(this.btWeatherGet);
             this.Controls.Add(this.tbWeatherInfo);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "天気予報表示アプリ";
+            
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,7 +81,7 @@ namespace WeatherApp {
 
         private System.Windows.Forms.TextBox tbWeatherInfo;
         private System.Windows.Forms.Button btWeatherGet;
-        private System.Windows.Forms.ComboBox cbPrefecture;
+        private System.Windows.Forms.ComboBox cbArea;
     }
 }
 
